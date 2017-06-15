e9c82e45e10f668ffea0587154d22db7223c55ab
塏翔 潘 <settour.frontend@gmail.com>
2017-06-15 10:54:06 +0800

    add the playground to test spread operator

---
+++ playground/spread.js
@@ -0,0 +1,38 @@
+/*
+let add = (a,b)=>{
+    return a + b;
+}
+
+let toAdd = [9,16];
+console.log(add(...toAdd));
+
+let groupA = ['Curry','Lebron'];
+let groupB = ['MCQ'];
+//將groupA跟groupB加入陣列final
+let final = ['Fiona'];
+console.log(final);
+
+//直接加入陣列
+final = ['Fiona',groupA,groupB];
+console.log(final);//[ 'Fiona',['Curry', 'Lebron'], ['MCQ'] ]
+
+//使用concat method，其實用不用剩餘參數都會是一樣結果
+final = final.concat(...groupA,...groupB);
+console.log(final);//[ 'Fiona', 'Curry', 'Lebron', 'MCQ' ]
+*/
+let person = ['paul',26];
+let personTwo = ['fiona',26];
+//Hi paul you are 26
+let greet = (name,age)=>{
+    console.log(`Hi ${name} your are ${age}`);
+}
+greet(...person);
+greet(...personTwo);
+let names =['Mike','Harden'];
+let final =['paul',...names];
+//Hi paul
+final.map(
+    (name)=>{
+       console.log(`Hi ${name}`) 
+    }
+)
