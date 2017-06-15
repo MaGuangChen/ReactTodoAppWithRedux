be66fe38ad7ad3f4b7b74f0983ce211e06cd70cc
塏翔 潘 <settour.frontend@gmail.com>
2017-06-15 16:35:07 +0800

    Add toggle support

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -12,4 +12,21 @@ describe('Todo component',()=>{
     it('測試Todo component是否存在',()=>{
        expect(Todo).toExist();
     });
+
+    it('應該接收onToggle prop並一路傳回TodoApp進行更新state',()=>{
+        let todoData = {
+            id:199,
+            text: '一些測試文字',
+            completed: true
+        };
+        //render component
+        let spy = expect.createSpy();
+        let todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy}/>);
+        //jq找到那個dom
+        let $el = $(ReactDOM.findDOMNode(todo));
+        //模擬todo被點擊
+        TestUtils.Simulate.click($el[0]);
+        //期望某個function被執行且
+        expect(spy).toHaveBeenCalledWith(199);
+    });
 });
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -23,4 +23,21 @@ describe('TodoApp component',()=>{
         expect(todoApp.state.todos[0].text).toBe('paul傳過來啦test test');
 
     });
+    it('透過handleToggle，應該toggle 已完成事項',()=>{
+        //一個測試用的object
+        let todoData = {
+            id:11,
+            text: 'Test features',
+            completed: false
+        };
+        //render上文件
+        let todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+        //設定state
+        todoApp.setState({todos: [todoData]});
+        //測試todoData中的first item裡面的completed為false
+        expect(todoApp.state.todos[0].completed).toBe(false);
+        //用id:11測試handleToggle
+        todoApp.handleToggle(11); 
+        expect(todoApp.state.todos[0].completed).toBe(true);
+    });
 });
