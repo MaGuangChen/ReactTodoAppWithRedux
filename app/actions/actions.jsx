43a45bfba46ed2b9c37594c64105091911d30db3
Paul Ma <kwn791122@gmail.com>
2017-09-07 23:03:01 +0800

    change the test var

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -1,29 +1,26 @@
-//searchText action generator
-export let setSearchText = (searchText)=>{
+
+export const setSearchText = (searchText) => {
     return {
        type: 'SET_SEARCH_TEXT',
-       searchText//等於searchText:searchText
+       searchText
     };
 }
 
-//toggleShowCompleted action generator
-//不用參數因為我們只有兩個state，要馬true要馬false
-export let toggleShowCompleted = ()=>{
+export const toggleShowCompleted = () => {
     return {
         type:'TOGGLE_SHOW_COMPLETED'
     }
 }
 
-//addTodo action generator
-export let addTodo = (text) => {
+export const addTodo = (text) => {
    return {
        type:'ADD_TODO',
        text
    };
 };
 
-//toggleTodo action generator
-export let toggleTodo = (id)=>{
+
+export let toggleTodo = (id) => {
     return {
         type:'TOGGLE_TODO',
         id
--- app/tests/action/action.test.jsx
+++ app/tests/action/action.test.jsx
@@ -1,40 +1,40 @@
 import expect from 'expect';
-var actions = require('actions');
+const actions = require('actions');
 
 describe('Actions',()=>{
     it('應該要產生search text action',()=>{
-        let action = {
+        const action = {
             type:'SET_SEARCH_TEXT',
             searchText: 'something else'
         };
         //等於這邊action obj帶入actions.jsx中的
-        let res = actions.setSearchText(action.searchText);
+        const res = actions.setSearchText(action.searchText);
         //期望res的值會跟action obj一樣
         expect(res).toEqual(action);
     });
      it('應該要產生add todo action',()=>{
-        let action = {
+        const action = {
             type:'ADD_TODO',
             text: '待辦事項1'
         };
         //等於這邊action obj帶入actions.jsx中的
-        let res = actions.addTodo(action.text);
+        const res = actions.addTodo(action.text);
         //期望res的值會跟action obj一樣
         expect(res).toEqual(action);
     });
     it('應該要產生toggle show completed action',()=>{
-       let action ={
+       const action ={
            type:'TOGGLE_SHOW_COMPLETED'
        }
-       let res = actions.toggleShowCompleted();
+       const res = actions.toggleShowCompleted();
        expect(res).toEqual(action);
     });
     it('應該要產生toggle to do action',()=>{
-       let action ={
+       const action = {
            type:'TOGGLE_TODO',
            id:1
        }
-       let res = actions.toggleTodo(action.id);
+       const res = actions.toggleTodo(action.id);
        expect(res).toEqual(action);
     });
 });
