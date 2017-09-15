c723d7dd3ee643de6318037499c502e6ad232c07
Paul Ma <kwn791122@gmail.com>
2017-09-15 20:31:47 +0800

    add toggle todo reducer and reducer test

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,5 +1,6 @@
 const expect = require('expect');
 const reducers = require('reducers');
+
 // 此套件為凍結一個物件不給改
 const deepFreeze = require('deep-freeze-strict');
 
@@ -25,5 +26,41 @@ describe('Reducers', () => {
           expect(res).toEqual(true);
       });
    });
+
+   describe('todos reducer', () => {
+       it('應該要新增新的todo', () => {
+         const action = {
+            type: 'ADD_TODO',
+            text: 'some text',
+         }
+         const res = reducers.todosReducer(deepFreeze([]), deepFreeze(action));
+         expect(res.length).toEqual(1);
+         expect(res[0].text).toEqual(action.text);
+       });
+
+       it('應該要將點選的todo切換完成或是未完成狀態', () => {
+           let todos = [
+               {
+                   id: '123',
+                   text: '測試用todos',
+                   completed: true,
+                   createdAt: 123,
+                   completedAt: 125,
+               },
+           ];
+           const action = {
+             type: 'TOGGLE_TODO',
+             id: '123',
+           };
+           const res = reducers.todosReducer(deepFreeze(todos), deepFreeze(action));
+           expect(res.length).toEqual(1);
+           expect(res[0].completed).toEqual(false);
+           expect(res[0].completedAt).toEqual(undefined);
+           expect(res[0].text).toEqual('測試用todos');
+       });
+   });
+
+  
+
   });
 });
